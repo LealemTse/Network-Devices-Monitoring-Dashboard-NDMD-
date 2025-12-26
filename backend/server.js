29300f398ae40f1cb441a7e9ec5ae41aa5c20528
Megdelawit365 <megdelawitaynalem365@gmail.com>
2025-12-26 16:16:49 +0300

    add integration with monitoring service

--- backend/server.js
+++ backend/server.js
@@ -2,27 +2,39 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const dbConnect = require('./config/db.js')
 const authRoutes = require('./routes/authRoutes.js')
+const deviceRoutes = require('./routes/deviceRoutes.js')
+const monitoringRoutes = require('./routes/monitoringRoutes.js')
+const dashboardRoutes = require('./routes/dashboardRoutes.js')
+
+const db = require('./config/db.js');
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/devices", deviceRoutes);
+app.use("/api/monitoring", monitoringRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+
+app.get('/test', (req, res) => {
+    res.send('server alive');
+});
+
 const testDBConnection = async () => {
     try {
-        const connection = await dbConnect()
-        console.log("Successfuly connected to mysql.");
+        const connection = await db.getConnection()
+        console.log("Successfuly connected to mysql.")
+        connection.release()
         app.listen(process.env.PORT, () => {
             console.log(`Listening on port ${process.env.PORT}`)
         })
-        connection.release();
     } catch (err) {
         console.log("Error connecting to mysql: ", err.message);
     }
 }
 testDBConnection();
-
