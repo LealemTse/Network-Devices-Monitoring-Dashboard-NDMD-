c4fe2588108a201ad596cd26b561b9f6a5e7b405
Megdelawit365 <megdelawitaynalem365@gmail.com>
2025-12-27 12:34:17 +0300

    remove files

--- backend/routes/configRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router()
-const { getRefreshInterval, updateRefreshInterval } = require('../controllers/configController')
-const authMiddleware = require('../middlewares/authMiddleWare')
-
-router.get('/refresh-interval', getRefreshInterval);
-router.put('/refresh-interval', authMiddleware, updateRefreshInterval);
-
-module.exports = router
--- backend/routes/logRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAllLogs, addLog } = require('../controllers/logsController');
-const authMiddleware = require('../middlewares/authMiddleWare')
-const router = express.Router()
-
-router.get('/', authMiddleware, getAllLogs)
-router.post('/', authMiddleware, addLog)
-
-module.exports = router
