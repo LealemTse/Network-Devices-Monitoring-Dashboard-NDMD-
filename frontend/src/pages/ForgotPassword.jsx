0becbfbf36bd07d748e828bb19f42216308cdf34
Bitaniya Damtew <bitaniyadamtew@gmail.com>
2026-01-06 11:54:25 +0300

    chore: added div for styling

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -7,31 +7,37 @@ export default function ForgotPassword() {
           Please verify your identity to reset your dashboard access. Select
           your security question and provied the answer.
         </p>
-        <label className="label" htmlFor="recovery-security-question">
-          Security Questions
-        </label>
-        <select
-          className=" recovery-security-questions"
-          name=""
-          id="recovery-security-question"
-        >
-          <option value="">Security Questions ...</option>
-          <option value="whats your 7th grade school name">
-            whats your 7th grade school name?
-          </option>
-          <option value="when is your mothers birthday">
-            when is your mothers birthday?
-          </option>
-          <option value="Where were you born">Where were you born?</option>
-        </select>
-        <label className="label" htmlFor="security-answer">
-          Your Answer
-        </label>
-        <input
-          type="password"
-          className="input security-answer"
-          placeholder="Type your answer here..."
-        />
+
+        <div className="form-group">
+          <label className="label" htmlFor="recovery-security-question">
+            Security Questions
+          </label>
+          <select
+            className=" recovery-security-questions"
+            name=""
+            id="recovery-security-question"
+          >
+            <option value="">Security Questions ...</option>
+            <option value="whats your 7th grade school name">
+              whats your 7th grade school name?
+            </option>
+            <option value="when is your mothers birthday">
+              when is your mothers birthday?
+            </option>
+            <option value="Where were you born">Where were you born?</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label className="label" htmlFor="security-answer">
+            Your Answer
+          </label>
+          <input
+            type="password"
+            className="input security-answer"
+            placeholder="Type your answer here..."
+          />
+        </div>
         <button className="forgot-submit"> Verify Me</button>
       </form>
     </div>
