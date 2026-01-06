23dfebc9c0083406df83897e413483b26a9fffd1
Bitaniya Damtew <bitaniyadamtew@gmail.com>
2026-01-06 12:01:15 +0300

    feat: add route for help page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
 import "./App.css";
 import "./index.css";
 import Login from "./pages/Login";
@@ -16,9 +14,10 @@ import {
 import RootLayout from "./layout/RootLayout";
 import Logs from "./pages/Logs.jsx";
 import Settings from "./pages/Settings.jsx";
+import Help from "./pages/Help.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -28,6 +27,7 @@ function App() {
         <Route path="forgot_password" element={<ForgotPassword />} />
         <Route path="logs" element={<Logs />} />
         <Route path="settings" element={<Settings />} />
+        <Route path="help" element={<Help />} />
       </Route>
     )
   );
